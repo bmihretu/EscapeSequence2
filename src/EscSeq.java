d0d9a3592b82f5f66aa670d3339acc218309ce34
bereket <motta4259@gmail.com>
2019-06-12 16:53:36 -0400

    first commit

---
+++ src/EscSeq.java
@@ -0,0 +1,11 @@
+public class EscSeq {
+    public static void main(String[] args){
+        System.out.print("Cost \t" + "\tQuantity \t" + "Total \n");
+        System.out.print("$1,000.00 \t" + "  4 \t" + "\t$4,000.00 \n");
+        System.out.print("$   50.00 \t" + "  8 \t" + "\t  $400.00 \n");
+        System.out.print("________________________________________\n");
+        System.out.print("TOTAL:    \t" + "  12\t" + "\t$4,440.00 \n");
+
+    }
+
+}
